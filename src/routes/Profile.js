7d0e7c596ef66532e87d0ecdf083d7406b309fd7
Essspressso <hologram1220@gmail.com>
2021-09-16 14:19:38 +0900

    feat: add logout funtion
    - function logout
    - redirect home using hook

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,17 @@
 import React from "react";
+import { authService } from "fBase";
+import { useHistory } from "react-router";
 
-const Profile = () => <span>Profile</span>
+const Profile = () => {
+    const history = useHistory();
+    const onLogoutClick = () => { 
+        authService.signOut();
+        history.push("/");
+    };
+    return (
+        <>
+            <button onClick={onLogoutClick}>Log out</button>
+        </>
+    );
+}
 export default Profile;
